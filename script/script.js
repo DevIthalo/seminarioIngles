a7c3e7b3c5976bd34aa3c131faaeb6d3dc52d2aa
Arthur <arthurpbliima@gmail.com>
2022-06-04 18:27:20 -0300

    Quase finalizando

--- script/script.js
+++ script/script.js
@@ -1,19 +1,20 @@
+/*
+    Nação Reggueira - TADS I
+ */
 
-    // Nação Reggueira - TADS I
-
-
+//Pegando elementos do HTML e atribuindo a variaveis
 var titulo = document.querySelector('.component-name')
 var conteudo = document.querySelector('.modal-content')
 var topico = document.querySelector('.modal-header')
 var assunto = document.querySelector('.subject')
 var subtitle = document.querySelector('.subtitle-modal')
-var passador = 0
+var passador = 0 // Posteriormente sera usada para passar e voltar os assuntos
 
-assunto.innerHTML = 'DATA MODEL'
-//PARA EVITAR REPETIÇÕES AO LONGO DO CODIGO - INICIO
-var arthurtitle = 'ARTHUR'
-var arthurcontent = '<desc id="arthur">Pass</desc> <desc id="arthur">the</desc> <desc id="arthur">mouse</desc>'
-var arthurheader = '<br>[ASSUNTO]'
+//INICIO DE TUDO QUE ESTARÁ NO MODAL
+assunto.innerHTML = 'DATA MODEL' // TEMA DA NAÇÃO REGGUEIRA que aparecerá no modal
+var arthurtitle = 'ARTHUR' // Nome dos integrantes que aparecerá no modal
+var arthurcontent = '<desc id="arthur">Pass</desc> <desc id="arthur">the</desc> <desc id="arthur">mouse</desc>' // ASSUNTO que estará no modal
+var arthurheader = '<br>[ASSUNTO]' // Nome do assunto que estará no modal
 
 var davititle = 'DAVI'
 var davicontent = '<desc id="davi">Pass</desc> <desc id="davi">the</desc> <desc id="davi">mouse</desc>'
@@ -48,22 +49,22 @@ function verConteudo(mn, pessoa){
     document.body.style.overflow = 'hidden'
     tads.innerHTML = ''
     if (pessoa === 'next'){
-        (passador === 0) ? passador++ : false
-        if (passador >= 1 && passador < 6) {
+        (passador === 0) ? passador++ : false // Se passador ser igual a 0 ele já incrementa para o 1
+        if (passador >= 1 && passador < 6) { // Passar os Cards
             passador++
         }
     }else if(pessoa === 'back'){
-        if (passador > 1 && passador <= 7) {
+        if (passador > 1 && passador <= 7) { // Voltar os cards
             passador--
         }
     }
-    for(let c = 1; c <= 6; c++) {
+    for(let c = 1; c <= 6; c++) { // Atribuir o numero da [pessoa] a variavel [passador]
         (pessoa === c) ? passador = c : false
     }
     if (pessoa === 1 || passador === 1){
         titulo.innerHTML = arthurtitle
         conteudo.innerHTML = arthurcontent
-        traducao('arthur')
+        traducao('arthur') // Chama a função de traduzir
         topico.innerHTML = arthurheader
     }else if (pessoa === 2 || passador === 2){
         titulo.innerHTML = davititle
@@ -105,7 +106,7 @@ function fecharConteudo(mn){
 }
 
 function traducao(nome){
-    var palavras = []
+    var palavras = [] // Inicializa a variavel vazia, para depois atribuir valores a ela
     if (nome === 'arthur'){
         palavras.push('<strong>Passe</strong>', '<strong>o</strong>', '<strong>mouse</strong>') // É AQUI QUE VEM AS PALAVRAS TRADUZIDAS!
     }else if(nome === 'davi'){
